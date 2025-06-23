3f0e2853ae5ea1ad732f592ea594e578c9acbe3f
DSA <mayurisharma2003@gmail.com>
2025-06-23 12:41:13 +0530

    ready

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,9 +9,9 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Smayuraa",
   resume:
-    "https://drive.google.com/file/d/1WLG0Ze6zvBJLUSk8BgRTYHMOUTfkrn42/view?usp=sharing",
+    "https://drive.google.com/file/d/1Yk2_PFxAcnm-ar3NRYFkgNKUWNjigKtW/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/sharma-mayuri/",
-  twitter: "https://twitter.com/RishavChanda",
+  twitter: "https://",
   insta: "https://www.instagram.com/mayuraa.vines/",
   facebook: "https://www.facebook.com/profile.php?id=100050627200522",
 };
